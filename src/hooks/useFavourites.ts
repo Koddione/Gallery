039dc092a592c47ef92f170f31bf6ccdfa665bc5
Koddione <135285665+Koddione@users.noreply.github.com>
2025-04-18 13:56:57 +0300

    feat: Добавлены кастомные хуки для разгрузки компонентов

---
+++ src/hooks/useFavourites.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { UnsplashPhoto } from '../types/unsplashPhoto';
+
+export const useFavourites = () => {
+	const [favouriteIds, setFavouriteIds] = useState<string[]>(() => {
+		const stored = JSON.parse(sessionStorage.getItem('favourites') || '[]');
+		return stored.map((p: UnsplashPhoto) => p.id);
+	});
+
+	const handleFavouriteChange = (photoId: string, isFavourite: boolean) => {
+		setFavouriteIds((prev) =>
+			isFavourite ? [...prev, photoId] : prev.filter((id) => id !== photoId),
+		);
+	};
+
+	return { favouriteIds, handleFavouriteChange };
+};
---
+++ src/hooks/useSearchParamsHandler.ts
@@ -0,0 +1,30 @@
+import { useSearchParams } from 'react-router-dom';
+import { SEARCH_PARAMS } from '../constants/searchParams';
+
+export const useSearchParamsHandler = () => {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const category = searchParams.get(SEARCH_PARAMS.CATERGORY) || '';
+	const page = parseInt(searchParams.get(SEARCH_PARAMS.PAGE) || '1');
+	const sortParamRaw = searchParams.get(SEARCH_PARAMS.SORT);
+	const sort: 'relevant' | 'latest' = sortParamRaw === 'latest' ? 'latest' : 'relevant';
+	const search = searchParams.get(SEARCH_PARAMS.SEARCH) || '';
+
+	const handleSortChange = (newSort: string) => {
+		setSearchParams({ category, page: '1', sort: newSort });
+	};
+
+	const handlePageChange = (newPage: number, totalPages: number) => {
+		if (newPage <= totalPages && newPage >= 1) {
+			setSearchParams({ category, page: newPage.toString() });
+		}
+	};
+
+	return {
+		category,
+		page,
+		sort,
+		search,
+		handleSortChange,
+		handlePageChange,
+	};
+};
