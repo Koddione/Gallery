a106822d7411e175f92b7411fc2031a8b90144a1
Koddione <135285665+Koddione@users.noreply.github.com>
2025-04-18 13:59:00 +0300

    feat: Добавлена утилита для разгрузки компанентов

---
+++ src/utils/favouritesStorage.ts
@@ -0,0 +1,21 @@
+import { STORAGE_KEY } from '../constants/storageKey';
+import { UnsplashPhoto } from '../types/unsplashPhoto';
+
+export const getFavourites = (): UnsplashPhoto[] => {
+	return JSON.parse(sessionStorage.getItem(STORAGE_KEY) || '[]');
+};
+
+export const saveFavourites = (items: UnsplashPhoto[]) => {
+	sessionStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+};
+
+export const toggleFavourite = (photo: UnsplashPhoto): boolean => {
+	const favourites = getFavourites();
+	const isAlreadySaved = favourites.some((fav) => fav.id === photo.id);
+	const updated = isAlreadySaved
+		? favourites.filter((fav) => fav.id !== photo.id)
+		: [...favourites, photo];
+
+	saveFavourites(updated);
+	return !isAlreadySaved;
+};
