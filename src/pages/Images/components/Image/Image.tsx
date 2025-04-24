4060766e3f89adba99102d3f0de4adc3892b43d4
Koddione <135285665+Koddione@users.noreply.github.com>
2025-04-24 17:12:43 +0300

    refactor: transferring the icon to assets, updated the import

--- src/pages/Images/components/Image/Image.tsx
+++ src/pages/Images/components/Image/Image.tsx
@@ -1,3 +1,4 @@
+import { CloseIco } from '../../../../assets/icons/CloseIco';
 import { Left } from '../../../../assets/icons/LeftArrow';
 import { Right } from '../../../../assets/icons/RightArrow';
 import { PhotoCard } from '../../../../components/PhotoCard/PhotoCard';
@@ -27,21 +28,7 @@ export const Image = ({
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <button className={styles.close} onClick={onClose}>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6 18L18 6M6 6L18 18"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <CloseIco />
           </button>
           <button className={styles.left} onClick={onPrev}>
             <Left />
